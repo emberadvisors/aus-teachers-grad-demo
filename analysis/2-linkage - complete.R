8a871a0beac926989aba294ba677ef3a9bbfc5bf
Bek Sawkins <bek.sawkins@emberadvisors.com.au>
2025-01-31 15:33:01 +1100

    Upload

---
+++ analysis/2-linkage - complete.R
@@ -0,0 +1,20 @@
+# Some recommended libraries
+library(tidyverse)
+library(readxl)
+library(janitor)
+
+# Some useful functions
+?read_rds()    # Read in your transformed datasets
+?inner_join()  # Methods for joining datasets together based on key columns
+?left_join()   
+?summarise()   # Helps with summarising data into smaller sets
+
+
+# Suggested linkage steps ----
+# Read in transformed data from step 1
+
+# Summarise datasets to have the same grain of information
+
+# Join data together
+
+# Save linked data as a single set for ease of future use
